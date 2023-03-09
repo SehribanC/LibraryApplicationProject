bf44e91d037c7eea8dca425e1750fe672398f655
Alisherb28 <alish.tussupbaev@gmail.com>
2023-03-08 14:20:20 -1000

    added jenkins

--- src/test/java/com/library/runners/CukesRunner.java
+++ src/test/java/com/library/runners/CukesRunner.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/library/step_definitions",
         dryRun = false,
-        tags = "@B28G1-109",
+        tags = "",
         publish = true
 )
 
